a45c382b26a3532c8b088ec32ebac94279a0fa96
Sophia <zwjc1@students.waikato.ac.nz>
2020-08-09 19:27:20 +1200

    Implement request handling

--- pod.py
+++ pod.py
@@ -12,7 +12,7 @@ from request import Request
 # status is a string that communicates the Pod's availability. ['PENDING','RUNNING', 'TERMINATING', 'FAILED']
 # the pool is the threads that are available for request handling on the pod
 class Pod:
-    def __init__(self, NAME, ASSIGNED_CPU, AVAILABLE_CPU, DEPLABEL):
+    def __init__(self, NAME, ASSIGNED_CPU, DEPLABEL):
         self.podName = NAME
         self.available_cpu = ASSIGNED_CPU
         self.deploymentLabel = DEPLABEL
@@ -21,14 +21,20 @@ class Pod:
         self.pool = ThreadPoolExecutor(max_workers=ASSIGNED_CPU)
         self.threads_running = 0
 
+    def has_capacity(self):
+        print(f'[Pod] {self.podName} is checking capacity {self.threads_running}/{self.available_cpu}')
+        return self.threads_running < self.available_cpu
+
     def HandleRequest(self, request: Request):
         def handler():
             self.threads_running += 1
+            print('[Pod] servicing request:', request.request_id)
             timed_out = self.crash.wait(timeout=request.execTime)
             if not timed_out:
                 # TODO -- log that this request crashed
                 pass
 
+            print('[Pod] finished request', request.request_id)
             self.threads_running -= 1
 
         self.pool.submit(handler)
--- req_handler.py
+++ req_handler.py
@@ -1,10 +1,13 @@
 import time
 
 from api_server import APIServer
+from pod import Pod
 
 # reqHandler is a thread that continuously checks the pendingRequest queue and calls an associated pod to handle the incoming request.
 
 class ReqHandler:
+    apiServer: APIServer
+
     def __init__(self, APISERVER):
         self.apiServer = APISERVER
         self.running = True
@@ -14,7 +17,25 @@ class ReqHandler:
         while self.running:
             self.apiServer.requestWaiting.wait()
             with self.apiServer.etcdLock:
-                pass
+                if not self.apiServer.etcd.pendingReqs: break
 
-            self.apiServer.requestWaiting.clear()
+                request = self.apiServer.etcd.pendingReqs.pop()
+                print('[ReqHandler] Got a request to service:', request.request_id)
+
+                candidate_endpoints = list(filter(
+                    lambda endpoint: self.apiServer.CheckEndPoint(endpoint),
+                    self.apiServer.GetEndPointsByLabel(request.deploymentLabel),
+                ))
+
+                for endpoint in candidate_endpoints:
+                    # First try to find a pod which can service the request immediately
+                    pod: Pod = endpoint.pod
+                    if pod.has_capacity():
+                        pod.HandleRequest(request)
+                        break
+                else:
+                    # TODO -- chuck this request onto the first endpoint (if there is one)
+                    print('[ReqHandler] Failed to find pod to service request', request.request_id)
+
+                self.apiServer.requestWaiting.clear()
         print("ReqHandlerShutdown")

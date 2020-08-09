724ecf31a957d0bed2c3f1887c404058aec6ce2a
Sophia <zwjc1@students.waikato.ac.nz>
2020-08-09 19:16:26 +1200

    assigned_cpu is an int, not a str

--- worker_node.py
+++ worker_node.py
@@ -7,6 +7,6 @@
 class WorkerNode:
     def __init__(self, INFOLIST):
         self.label = INFOLIST[0]
-        self.assigned_cpu = INFOLIST[1]
+        self.assigned_cpu = int(INFOLIST[1])
         self.available_cpu = self.assigned_cpu
         self.status = 'UP'

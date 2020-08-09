e45a9d43a228804d23deb7bbf7cfd2b2f0686e24
Sophia <zwjc1@students.waikato.ac.nz>
2020-08-09 19:17:21 +1200

    Fix Request constructor

--- request.py
+++ request.py
@@ -3,7 +3,7 @@
 # cpuCost is the number of threads that the request will use on a pod
 # duration is how long the request will use those resource for before completing
 class Request:
-    def __init__(self, APISERVER, INFOLIST):
-        self.deploymentLabel = INFOLIST[0]
-        self.cpuCost = INFOLIST[1]
-        self.execTime = INFOLIST[3]
+    def __init__(self, INFOLIST):
+        self.request_id = INFOLIST[0]
+        self.deploymentLabel = INFOLIST[1]
+        self.execTime = int(INFOLIST[2])

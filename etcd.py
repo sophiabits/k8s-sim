2447371402de66f9c55fd2f9b11284695217c96a
Sophia <zwjc1@students.waikato.ac.nz>
2020-08-09 18:34:20 +1200

    Type hints for etcd

--- etcd.py
+++ etcd.py
@@ -1,22 +1,30 @@
 from concurrent.futures import ThreadPoolExecutor
 import threading
+from typing import List
 
-from pod import Pod
 from deployment import Deployment
 from end_point import EndPoint
+from pod import Pod
+from request import Request
 from worker_node import WorkerNode
 
-# Etcd is the storage component of the cluster that allows for comparison between expected
-# configuration and real time information.
-# pendingPodList is a list of Pod objects that have been created by the Deployment Controller but have yet to
-# be scheduled.
-# deploymentList is a list of Deployment Objects.
-# nodeList is a list of the nodes within the cluster. in this assignment this consists only of WorkerNodes.
-# endPointList is a list of EndPoint objects.
 class Etcd:
+    '''Etcd is the storage component of the cluster that allows for comparison between expected configuration and real time information.'''
+
+    deploymentList: List[Deployment]
+    endPointList: List[EndPoint]
+    # List of the nodes within the cluster
+    nodeList: List[WorkerNode]
+    # List of pods created by the deployment controller which are waiting to be scheduled
+    pendingPodList: List[Pod]
+    runningPodList: List[Pod]
+
+    pendingReqs: List[Request]
+    reqCreator: ThreadPoolExecutor
+
     def __init__(self):
         self.pendingPodList = []
-        self.runningPodList= []
+        self.runningPodList = []
         self.deploymentList = []
         self.nodeList = []
         self.endPointList = []

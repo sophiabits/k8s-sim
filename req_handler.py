8fcb5e28be1e1ef6c0a67209b8578a020bbddfe3
Sophia <zwjc1@students.waikato.ac.nz>
2020-08-13 19:05:51 +1200

    Fix request routing

--- req_handler.py
+++ req_handler.py
@@ -22,23 +22,27 @@ class ReqHandler:
 
                 request_info = self.apiServer.etcd.pendingReqs.pop()
                 request = Request(request_info)
-                print('[ReqHandler] Got a request to service:', request)
 
                 candidate_endpoints = list(filter(
                     lambda endpoint: self.apiServer.CheckEndPoint(endpoint),
                     self.apiServer.GetEndPointsByLabel(request.deploymentLabel),
                 ))
 
-                for endpoint in candidate_endpoints:
-                    # First try to find a pod which can service the request immediately
-                    pod: Pod = endpoint.pod
-                    if pod.has_capacity():
-                        pod.HandleRequest(request)
-                        break
-                else:
+                if not candidate_endpoints:
                     # In reality, we'd probably put this request back onto the queue
                     # But Stephen has said it's OK to just fail the request when there isn't an available pod.
                     print('[ReqHandler] Failed to find pod to service request', request)
+                else:
+                    for endpoint in candidate_endpoints:
+                        # First try to find a pod which can service the request immediately
+                        pod: Pod = endpoint.pod
+                        if pod.has_capacity():
+                            pod.HandleRequest(request)
+                            break
+                    else:
+                        # No pods can service the request immediately, so just chuck it on the first pod
+                        pod = candidate_endpoints[0].pod
+                        pod.HandleRequest(request)
 
                 self.apiServer.requestWaiting.clear()
         print("ReqHandlerShutdown")

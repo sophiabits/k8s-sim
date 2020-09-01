5f49254199a1d96bf4ad180616abdb904ea5cfa8
Sophia Willows <zwjc1@students.waikato.ac.nz>
2020-09-01 14:26:11 +1200

    Round robin load balancer implementation

--- load_balancing.py
+++ load_balancing.py
@@ -19,16 +19,23 @@ class ILoadBalancer:
         print('LoadBalancer start')
         while self.running:
             self.deployment.waiting.wait()
-            with self.deployment.lock:
-                request_infos = self.deployment.pendingReqs
-                self.deployment.pendingReqs = []
+            self.deployment.waiting.clear()
 
             with self.api_server.etcdLock:
                 candidate_endpoints = self.api_server.GetEndPointsByLabel(self.deployment.deploymentLabel)
 
-            # TODO -- what happens if no candidate_endpoints? drop the req?
-
             pods = [endpoint.pod for endpoint in candidate_endpoints]
+            if not pods:
+                # No pods available to handle the request for this deployment, so we'll skip this iteration
+                # TODO -- does this impl actually work?
+                # TODO -- metric
+                self.deployment.waiting.set()
+                continue
+
+            with self.deployment.lock:
+                request_infos = self.deployment.pendingReqs
+                self.deployment.pendingReqs = []
+
             for info in request_infos:
                 request = Request(info)
                 self.handle(pods, request)
@@ -39,9 +46,20 @@ class ILoadBalancer:
     def handle(self, pods, request): raise NotImplementedError
 
 class RoundRobinLoadBalancer(ILoadBalancer):
+    ''' Tracks which pod index to assign the next incoming request to. '''
+    current_index = 0
+
     # Cycle through pods in a deployment
     def handle(self, pods, request):
-        pass
+        if self.current_index >= len(pods):
+            # Wrap around back to the first pod
+            self.current_index = 0
+
+        pod = pods[self.current_index]
+        metrics.request_routed(pod, request)
+        pod.HandleRequest(request)
+
+        self.current_index += 1
 
 class UtilizationAwareLoadBalancer(ILoadBalancer):
     # Send request to lowest utilized pod
